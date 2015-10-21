d71baf99e2c755f7de47121df644a2b4dcdd3924
Tom Coleman <tom@thesnail.org>
2015-10-21 11:41:29 +1100

    Works for 1.2

--- package.js
+++ package.js
@@ -1,4 +1,5 @@
 Package.describe({
+  name: 'dburles:spacebars-tohtml',
   summary: "Easily compile spacebars templates to html",
   version: "1.0.0",
   git: "https://github.com/dburles/meteor-spacebars-tohtml.git"
@@ -6,6 +7,6 @@ Package.describe({
 
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@0.9.0');
-  api.use(['spacebars@1.0.0', 'spacebars-compiler@1.0.1']);
+  api.use(['spacebars@1.0.0', 'spacebars-compiler@1.0.1', 'blaze@2.1.3']);
   api.addFiles('spacebars-tohtml.js');
 });
